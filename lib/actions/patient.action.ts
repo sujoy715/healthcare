0f9552dc1f8219b92b7b4d6598bbbf30c45d83a9
sujoy715 <sarkarsujoy715@gmail.com>
2024-09-02 12:05:15 +0530

    first commit

---
+++ lib/actions/patient.action.ts
@@ -0,0 +1,26 @@
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+
+export const createUser = async (user: CreateUserParams)=>{
+    try {
+        // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+        const newuser = await users.create(
+          ID.unique(),
+          user.email,
+          user.phone,
+          undefined,
+          user.name
+        );
+    
+        console.log({newuser})
+      } catch (error: any) {
+        // Check existing user
+        if (error && error?.code === 409) {
+          const existingUser = await users.list([
+            Query.equal("email", [user.email]),
+          ]);
+    
+          return existingUser.users[0];
+        }
+    }
+}
